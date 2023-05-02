4754c7009e9d735567de327d7ed006faf7c7b865
Elgun837 <elgun.xalilov@bk.ru>
2023-05-02 12:36:32 +0400

    home-animation

---
+++ src/components/AIPicker.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AIPicker = () => {
+  return (
+    <div>Alpicker</div>
+  )
+}
+
+export default AIPicker
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton  from "./CustomButton";
+import FilePicker  from "./FilePicker";
+import Tab  from "./Tab";
+import ColorPicker from "./Colorpicker";
+import AIPicker from "./AIpicker";
+
+export {CustomButton,AIPicker,FilePicker,Tab,ColorPicker};
