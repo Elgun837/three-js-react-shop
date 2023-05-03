8b9247c4477521fa41cdb16215fb4393798584c1
Elgun837 <elgun.xalilov@bk.ru>
2023-05-03 15:47:53 +0400

    customButton

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -28,13 +28,50 @@ const Customizer = () => {
       case "colorpicker":
         return <ColorPicker />
       case "filepicker":
-        return <FilePicker />
+        return <FilePicker 
+        file={file}
+        setFile={setFile}
+        readFile={readFile}
+        />
       case "aipicker":
         return <AIPicker />
       default:
-        break;
+        return null;
     }
   }
+
+
+  const handleDecals = (type,result) => {
+    const decalType = DecalTypes[type];
+
+    state[decalType.stateProperty] = result;
+
+    if(!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab)
+    }
+  }
+
+  const handleActiveFilterTab = (tabName) => {
+    switch(tabName) {
+      case "logoShirt":
+        state.isLogoTexture = !setActiveFilterTab[tabName];
+        break;
+      case "stylishShirt":
+        state.isFullTexture = !activeFilterTab[tabName];
+      default:
+          state.isFullTexture = true;
+          state.isLogoTexture = true;
+    }
+  }
+
+  const readFile = (type) => {
+    reader(file)
+    .then((result) => {
+      handleDecals(type,result);
+      setActiveEditorTab("");
+    })
+  }
+
 
   return (
     <AnimatePresence>
